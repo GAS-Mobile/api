9eccc1179f7542baa0161143fb2e482e62b19676
Igor <isidoroigor9@gmail.com>
2023-08-12 23:48:23 -0300

    Endpoints para manipular analistas

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const customerRoutes = require('./customerRoutes')
 const companyRoutes = require('./companyRoutes')
+const analystRoutes = require('./analystRoutes')
 
 router.use('/customers', customerRoutes)
 router.use('/companies', companyRoutes)
+router.use('/analysts', analystRoutes)
 
 module.exports = router

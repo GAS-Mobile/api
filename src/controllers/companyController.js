19ffd37256fb9f100867223bb17cb17463750fa1
Igor <isidoroigor9@gmail.com>
2023-08-17 00:38:31 -0300

    Alterações no CRUD dos models 'Analyst', 'Company' e 'Customer' para que fiquem de acordo com o novo esquema do BD

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -1,9 +1,10 @@
 const { Company } = require('../models/Company')
+
 // Public route
 const getAllCompanies = async (req, res) => {
   try {
     const companies = await Company.find()
-      .select({__v: 0, _id: 0, 'headquartersLocation._id': 0})
+      .select({__v: 0, 'headquartersLocation._id': 0})
       
     res.json({companies})
 
@@ -11,87 +12,130 @@ const getAllCompanies = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
 // Private route for ????
 const createCompany = async (req, res) => {
   try {
     const data = req.body.company
+
+    if (!data || !data.name || !data.industry || !data.cnpj || !data.headquartersLocation ||
+        !data.headquartersLocation.street || !data.headquartersLocation.city || 
+        !data.headquartersLocation.state || !data.headquartersLocation.postalCode || 
+        !data.headquartersLocation.country
+    ){
+      return res.status(400).json({ 
+        message: 'To create a company is necessary to send name, industry, CNPJ and headquartersLocation.'
+      })
+    }
     
     const companyExists = await Company.exists({ cnpj: data.cnpj })
     if(companyExists) {
-      return res.status(409).json({ message: 'Company already exists' })
+      return res.status(409).json({ message: 'CNPJ is already in use by another company' })
     }
 
-    await Company.create(data)
+    await Company.create({
+      name: data.name,
+      industry: data.industry,
+      cnpj: data.cnpj,
+      headquartersLocation: data.headquartersLocation,
+    })
+
     res.status(201).json({ message: 'Company created successfully' })
 
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
+
 // Public route
 const getCompanyByID = async (req, res) => {
   try {
     const companyID = req.params.id
 
-    if (companyID.length !== 24) {
+    if (!companyID || companyID.length !== 24) {
       return res.status(404).json({ message: 'Company not found' })
     }
 
     const company = await Company.findById(companyID)
-      .select({__v: 0, _id: 0, 'headquartersLocation._id': 0})
+      .select({__v: 0, 'headquartersLocation._id': 0})
 
     if (!company) {
       return res.status(404).json({ message: 'Company not found' })
     }
 
     res.status(200).json({company})
-
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
+
 // Private route for analysts
 const updateCompanyByID = async (req, res) => {
   try {
     const companyID = req.params.id
     const data = req.body.company
 
-    if (companyID.length !== 24) {
+    if (!data || (!data.name && !data.industry && !data.cnpj && (!data.headquartersLocation 
+      || (!data.headquartersLocation.street && !data.headquartersLocation.city &&
+      !data.headquartersLocation.state && !data.headquartersLocation.postalCode && 
+      !data.headquartersLocation.country)))
+    ){
+      return res.status(400).json({ 
+        message: 'To update a company, at least one field (name, industry, CNPJ, headquartersLocation) must be provided' 
+      })
+    }
+
+    if (!companyID || companyID.length !== 24) {
       return res.status(404).json({ message: 'Company not found' })
     }
 
     const company = await Company.findById(companyID)
-
     if (!company) {
       return res.status(404).json({ message: 'Company not found' })
     }
 
-    company.set(data)
-    await company.save()
-    res.status(200).json({ message: 'Company updated successfully' })
+    if (data.cpnj) {
+      const companyWithNewCNPJ = await Company.findOne({ cnpj: data.cnpj })
+      if (companyWithNewCNPJ && companyWithNewCNPJ._id.toString() !== company._id.toString()) {
+        return res.status(409).json({ message: 'CNPJ is already in use by another company' })
+      }
+    }
 
+    if (data.name) company.name = data.name
+    if (data.industry) company.industry = data.industry
+    if (data.cnpj) company.cnpj = data.cnpj
+    if (data.headquartersLocation) {
+      if (data.headquartersLocation.street) company.headquartersLocation.street = data.headquartersLocation.street
+      if (data.headquartersLocation.city) company.headquartersLocation.city = data.headquartersLocation.city
+      if (data.headquartersLocation.state) company.headquartersLocation.state = data.headquartersLocation.state
+      if (data.headquartersLocation.postalCode) company.headquartersLocation.postalCode = data.headquartersLocation.postalCode
+      if (data.headquartersLocation.country) company.headquartersLocation.country = data.headquartersLocation.country
+    }
+
+    await company.save()
+
+    res.status(200).json({ message: 'Company updated successfully' })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
+
 // Private route for admins
 const deleteCompanyByID = async (req, res) => {
   try {
     const companyID = req.params.id
 
-    if (companyID.length !== 24) {
+    if (!companyID || companyID.length !== 24) {
       return res.status(404).json({ message: 'Company not found' })
     }
 
     const companyExists = await Company.exists({ _id: companyID })
-
     if (!companyExists) {
       return res.status(404).json({ message: 'Company not found' })
     }
 
     await Company.deleteOne({ _id: companyID})
     res.status(200).json({ message: 'Company deleted successfully' })
-
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,24 +1,40 @@
+const { User } = require('../models/User')
 const { Customer } = require('../models/Customer')
 const { Analyst } = require('../models/Analyst')
+
 // Public route
 const createCustomer = async (req, res) => {
   try {
     const data = req.body.customer
 
-    const customerExists = await Customer
-      .exists({$or: [{email: data.email}, {cpf: data.cpf}]})
+    if (!data || !data.email || !data.password || !data.name || !data.cpf){
+      return res.status(400).json({ 
+        message: 'To create a customer is necessary to send email, password, name and CPF'
+      })
+    }
 
+    const userExists = await User.exists({ email: data.email })
+    if (userExists) {
+      return res.status(409).json({ message: 'Email is already in use by another user' })
+    }
+
+    const customerExists = await Customer.exists({cpf: data.cpf})
     if (customerExists) {
-      return res.status(409).json({ message: 'Customer already exists' })
+      return res.status(409).json({ message: 'CPF is already in use by another user' })
     }
 
     const analystExists = await Analyst.exists({cpf: data.cpf})
     if (analystExists) {
-      return res.status(409)
-        .json({ message: 'There is already an account with the cpf inserted'})
+      return res.status(409).json({ message: 'CPF is already in use by another user' })
     }
+    
+    const newUser = await User.create({email: data.email, password: data.password})
+    await Customer.create({
+      user: newUser._id,
+      name: data.name,
+      cpf: data.cpf
+    })
 
-    await Customer.create(data)
     res.status(201).json({ message: 'Customer created successfully' })
 
   } catch (error) {
@@ -29,7 +45,8 @@ const createCustomer = async (req, res) => {
 const getCustomers = async (req, res) => {
   try {
     const customers = await Customer.find()
-      .select({_id: 0, __v:0, password: 0})
+      .populate('user', 'email _id')
+      .select({__v: 0})
       
     res.status(200).json({customers})
 
@@ -37,82 +54,98 @@ const getCustomers = async (req, res) => {
     res.status(500).json({ message: 'An error occurred while fetching customers' })
   }
 }
+
 // Private route for customers
 const getCustomerByID = async (req, res) => {
   try {
     const customerID = req.params.id
 
-    if (customerID.length !== 24) {
+    if (!customerID || customerID.length !== 24) {
       return res.status(404).json({ message: 'Customer not found' })
     }
 
     const customer = await Customer.findById(customerID)
-      .select({_id: 0, __v:0, password: 0})
+      .populate('user', 'email _id')
+      .select({__v: 0})
 
     if (!customer) {
       return res.status(404).json({ message: 'Customer not found' })
     }
 
     res.status(200).json({customer})
-
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
+
 // Private route for customers
 const updateCustomerByID = async (req, res) => {
   try {
     const customerID = req.params.id
     const data = req.body.customer
 
-    if (customerID.length !== 24) {
+    if (!data || (!data.email && !data.password && !data.name && !data.cpf)) {
+      return res.status(400).json({ 
+        message: 'To update a customer, at least one field (email, password, name, cpf) must be provided' 
+      })
+    }
+
+    if (!customerID || customerID.length !== 24) {
       return res.status(404).json({ message: 'Customer not found' })
     }
 
-    const customer = await Customer.findById(customerID)
-
+    const customer = await Customer.findById(customerID).populate('user', 'email _id')
     if (!customer) {
       return res.status(404).json({ message: 'Customer not found' })
     }
 
-    const customerWithEmailReceived = await Customer.findOne({ email: data.email })
-
-    if (!customerWithEmailReceived) {
-      customer.set(data)
-      await customer.save()
-      res.status(200).json({ message: 'Customer updated successfully' })
-
-    } else if(customerWithEmailReceived.cpf === customer.cpf){
-      customer.set(data)
-      await customer.save()
-      res.status(200).json({ message: 'Customer updated successfully' })
-
-    } else {
-      res.status(409).json({ message: 'A customer with this email already exists' })
+    if (data.email) {
+      const userWithNewEmail = await User.findOne({ email: data.email })
+      if (userWithNewEmail && userWithNewEmail._id.toString() !== customer.user._id.toString()) {
+        return res.status(409).json({ message: 'Email is already in use by another user' })
+      }
     }
 
+    if (data.cpf) {
+      const customerWithNewCPF = await Customer.findOne({ cpf: data.cpf })
+      const analystWithNewCPF = await Analyst.findOne({ cpf: data.cpf })
+      if ((customerWithNewCPF && customerWithNewCPF._id.toString() !== customerID) || analystWithNewCPF) {
+        return res.status(409).json({ message: 'CPF is already in use by another user' })
+      }
+    }
+
+    if (data.email) customer.user.email = data.email
+    if (data.password) customer.user.password = data.password
+    if (data.name) customer.name = data.name
+    if (data.cpf) customer.cpf = data.cpf
+
+    await customer.save()
+    await customer.user.save()
+
+    res.status(200).json({ message: 'Customer updated successfully' })
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'An error occurred while updating the customer' })
   }
 }
+
 // Private route for customers
 const deleteCustomerByID = async (req, res) => {
   try {
     const customerID = req.params.id
 
-    if (customerID.length !== 24) {
+    if (!customerID || customerID.length !== 24) {
       return res.status(404).json({ message: 'Customer not found' })
     }
 
-    const customerExists = await Customer.exists({ _id: customerID })
-
-    if (!customerExists) {
+    const customer = await Customer.findById(customerID)
+    if (!customer) {
       return res.status(404).json({ message: 'Customer not found' })
     }
 
     await Customer.deleteOne({ _id: customerID})
-    res.status(200).json({ message: 'Customer deleted successfully' })
+    await User.deleteOne({ _id: customer.user._id})
 
+    res.status(200).json({ message: 'Customer deleted successfully' })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
